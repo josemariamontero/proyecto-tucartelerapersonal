8d2f8e93f944d6ea9ec0915a2ece67aaf0e76f15
Jose Maria Montero <josema9817@gmail.com>
2019-05-14 20:42:56 +0200

    No muestra resultados

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ url_base = "https://api.themoviedb.org/3"
 @app.route('/',methods=["GET","POST"])
 def inicio():
 	payload = {"api_key":"key","language":"es-ES"}
-	r = requests.get(url_base+"/movie/popular",params=payload)
+	r = requests.get("https://api.themoviedb.org/3/movie/popular",params=payload)
 	if r.status_code == 200:
 		doc = r.json()
 		novedades = doc["results"]
@@ -20,11 +20,12 @@ def inicio():
 def peliculas():
 	peliculas = request.form.get('pelicula')
 	payload2 = {"api_key":"key","language":"es-ES","query":peliculas}
-	r2 = requests.get(url_base+"/search/movie/",params=payload2)
+	r2 = requests.get("https://api.themoviedb.org/3/search/movie",params=payload2)
 	if r2.status_code == 200:
-		doc2 = r2.json()
-		peliculas = doc2["results"]
-		return render_template("peliculas.html",peliculas=peliculas)
+		doc = r2.text
+		pelis = json.loads(doc)
+		peliculas = pelis["results"]
+	return render_template("peliculas.html",peliculas=peliculas)
 		
 
 if __name__ == '__main__':
