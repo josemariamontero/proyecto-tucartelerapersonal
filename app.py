f6e1e03b82d78c215875f7fa805f7015e81f450f
Jose Maria Montero <josema9817@gmail.com>
2019-05-16 16:29:47 +0200

    Añadido ultimas novedades y series

--- app.py
+++ app.py
@@ -8,24 +8,34 @@ key = os.environ["key"]
 url_base = "https://api.themoviedb.org/3"
 
 @app.route('/',methods=["GET","POST"])
-def inicio():
-	payload = {"api_key":"key","language":"es-ES"}
-	r = requests.get("https://api.themoviedb.org/3/movie/popular",params=payload)
+@app.route('/<page>')
+def inicio(page=1):
+	payload = {"api_key":key,"language":"es-ES","page":page}
+	r = requests.get(url_base+"/movie/popular",params=payload)
 	if r.status_code == 200:
 		doc = r.json()
 		novedades = doc["results"]
-	return render_template("inicio.html")
+	return render_template("inicio.html",novedades=novedades,page=int(page)+1)
 
 @app.route('/busquedapeliculas',methods=["GET","POST"])
 def peliculas():
 	peliculas = request.form.get('pelicula')
-	payload2 = {"api_key":"key","language":"es-ES","query":peliculas}
-	r2 = requests.get("https://api.themoviedb.org/3/search/movie",params=payload2)
+	payload2 = {"api_key":key,"language":"es-ES","query":peliculas}
+	r2 = requests.get(url_base+"/search/movie",params=payload2)
 	if r2.status_code == 200:
-		doc = r2.text
-		pelis = json.loads(doc)
-		peliculas = pelis["results"]
+		doc2 = r2.json()
+		peliculas = doc2["results"]
 	return render_template("peliculas.html",peliculas=peliculas)
+
+@app.route('/busquedaseries',methods=["GET","POST"])
+def series():
+	series = request.form.get('serie')
+	payload3 = {"api_key":key,"language":"es-ES","query":series}
+	r3 = requests.get(url_base+"/search/tv",params=payload3)
+	if r3.status_code == 200:
+		doc3 = r3.json()
+		series = doc3["results"]
+	return render_template("series.html",series=series)
 		
 
 if __name__ == '__main__':
