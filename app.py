9767dc9ad25f0045cba68c8ea0f2be148801828a
Jose Maria Montero <josema9817@gmail.com>
2019-05-23 20:44:22 +0200

    Twitter funcionando

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def vertweet():
                    resource_owner_key=access_token,
                    resource_owner_secret=access_token_secret)
     url = 'https://api.twitter.com/1.1/statuses/update.json'
-    payload={"status":"He twitteado sobre una pelicula en https://tucartelerapersonal.herokuapp.com!"}
+    payload={"status":"Si necesitas conocer información sobre películas visita ésta página web: https://tucartelerapersonal.herokuapp.com."}
     r = requests.post(url=url,auth=oauth,params=payload)
     if r.status_code==200:
         return render_template("twittear.html",datos=r.json())
